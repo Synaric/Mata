bba2269c0e4eeb41313f7932b493e18870af09b6
Synaric <gcy2014@126.com>
2016-10-08 15:20:41 +0800

    fix wrong buildConfigField "HOST"

--- app/src/main/java/com/synaric/app/mata/service/StandardModel.java
+++ app/src/main/java/com/synaric/app/mata/service/StandardModel.java
@@ -3,7 +3,10 @@ package com.synaric.app.mata.service;
 import com.synaric.app.mata.BuildConfig;
 
 import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 import rx.Observable;
 
 /**
@@ -17,17 +20,17 @@ public interface StandardModel {
     /**
      * 普通网络访问URL。
      */
-    String BASE_URL = "http://" + BuildConfig.HOST + "/mata-service/dispatcher";
+    String BASE_URL = "http://" + BuildConfig.HOST + "/mata-service/dispatcher/";
 
     /**
      * 检查是否能成功请求服务端。成功则返回"request: ok"。
      */
-    @POST("/emptyService")
-    Observable<String> emptyRequest(@Field("params") String params, @Field("ext") String ext);
+    @GET("emptyService")
+    Observable<String> emptyRequest(@Query("params") String params, @Query("ext") String ext);
 
     /**
      * 用户登录。
      */
-    @POST("/userService")
-    Observable<String> login(@Field("params") String params, @Field("ext") String ext);
+    @GET("userService")
+    Observable<String> login(@Query("params") String params, @Query("ext") String ext);
 }
